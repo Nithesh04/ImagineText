1144d366750499710d4916945575846038d4fea2
Nithesh G <158061090+Nithesh04@users.noreply.github.com>
2025-03-15 20:46:03 +0530

    Update SubHeading.tsx

--- client/src/components/SubHeading.tsx
+++ client/src/components/SubHeading.tsx
@@ -1,6 +1,6 @@
 const SubHeading = ({ content }: { content: string }) => {
   return (
-    <h2 className="text-2xl text-center mt-2 opacity-90 leading-relaxed">
+    <h2 className="text-1.5xl text-center mt-2 opacity-90 leading-relaxed">
       Introducing Imagine Text💡 which converts images into text effortlessly using OCR technology!
       <br />
       Unleash the power of visualization! 💡📷🔍
